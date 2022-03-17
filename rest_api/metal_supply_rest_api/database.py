119aa59ea316a92a749488cef8d37ea73763a128
Kaloyan Terziev <kaloyan.terziev@student.manchester.ac.uk>
2022-03-17 15:09:29 +0000

    Use Connection Pool instead of a connection

--- rest_api/metal_supply_rest_api/database.py
+++ rest_api/metal_supply_rest_api/database.py
@@ -35,7 +35,7 @@ class Database(object):
         self._dsn = 'dbname={} user={} password={} host={} port={}'.format(
             name, user, password, host, port)
         self._loop = loop
-        self._conn = None
+        self._pool = None
 
     async def connect(self, retries=5, initial_delay=1, backoff=2):
         """Initializes a connection to the database
@@ -50,7 +50,7 @@ class Database(object):
         delay = initial_delay
         for attempt in range(retries):
             try:
-                self._conn = await aiopg.connect(
+                self._pool = await aiopg.create_pool(
                     dsn=self._dsn, loop=self._loop, echo=True)
                 LOGGER.info('Successfully connected to database')
                 return
@@ -63,14 +63,14 @@ class Database(object):
                 await asyncio.sleep(delay)
                 delay *= backoff
 
-        self._conn = await aiopg.connect(
+        self._pool = await aiopg.connect(
             dsn=self._dsn, loop=self._loop, echo=True)
         LOGGER.info('Successfully connected to database')
 
     def disconnect(self):
         """Closes connection to the database
         """
-        self._conn.close()
+        self._pool.close()
 
     async def create_auth_entry(self,
                                 public_key,
@@ -88,10 +88,11 @@ class Database(object):
             encrypted_private_key.hex(),
             hashed_password.hex())
 
-        async with self._conn.cursor() as cursor:
-            await cursor.execute(insert)
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(insert)
 
-        self._conn.commit()
+        self._pool.commit()
 
     async def create_agent_entry(self, public_key, name, email):
         insert = """
@@ -103,10 +104,11 @@ class Database(object):
                 VALUES ('{}', '{}', '{}');
                 """.format(
             public_key, name, email)
-        async with self._conn.cursor() as cursor:
-            await cursor.execute(insert)
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(insert)
 
-        self._conn.commit()
+        self._pool.commit()
 
     async def fetch_agent_resource(self, agent_id):
         fetch = """
@@ -117,9 +119,10 @@ class Database(object):
         AND ({1}) < agents.end_block_num;
         """.format(agent_id, LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchone()
 
     async def fetch_all_agent_resources(self):
         fetch = """
@@ -129,9 +132,10 @@ class Database(object):
         AND ({0}) < agents.end_block_num;
         """.format(LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchall()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchall()
 
     async def fetch_agent_public_key(self, agent_id):
         fetch = """
@@ -139,9 +143,10 @@ class Database(object):
             FROM agents
             WHERE id = {0};
         """.format(agent_id)
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchone()
 
     async def fetch_agent_on_email(self, email):
         fetch = """
@@ -151,18 +156,20 @@ class Database(object):
         WHERE agents.email='{}'
         """.format(email)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchone()
 
     async def fetch_auth_resource(self, public_key):
         fetch = """
         SELECT * FROM auth WHERE public_key='{}'
         """.format(public_key)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchone()
 
     async def fetch_record_public_key(self, record_id):
         fetch = """
@@ -170,9 +177,10 @@ class Database(object):
                    FROM records
                    WHERE id = {0};
                """.format(record_id)
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            await cursor.execute(fetch)
-            return await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(fetch)
+                return await cursor.fetchone()
 
     async def create_record_entry(self, record_public_key, material_type, material_origin, tonnes, contents, published):
         insert_records = """
@@ -201,12 +209,13 @@ class Database(object):
         ]
         logging.debug(insert_records)
         logging.debug(insert_record_contents)
-        async with self._conn.cursor() as cursor:
-            await cursor.execute(insert_records)
-            for insert in insert_record_contents:
-                await cursor.execute(insert)
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                await cursor.execute(insert_records)
+                for insert in insert_record_contents:
+                    await cursor.execute(insert)
 
-        self._conn.commit()
+        self._pool.commit()
 
     #TODO: delete
     async def fetch_record_resource(self, record_id):
@@ -231,20 +240,21 @@ class Database(object):
         AND ({1}) < end_block_num;
         """.format(record_id, LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            try:
-                await cursor.execute(fetch_record)
-                record = await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                try:
+                    await cursor.execute(fetch_record)
+                    record = await cursor.fetchone()
 
-                await cursor.execute(fetch_record_locations)
-                record['locations'] = await cursor.fetchall()
+                    await cursor.execute(fetch_record_locations)
+                    record['locations'] = await cursor.fetchall()
 
-                await cursor.execute(fetch_record_owners)
-                record['owners'] = await cursor.fetchall()
+                    await cursor.execute(fetch_record_owners)
+                    record['owners'] = await cursor.fetchall()
 
-                return record
-            except TypeError:
-                return None
+                    return record
+                except TypeError:
+                    return None
 
     async def fetch_record_resource(self, record_public_key, agent_id):
         fetch_record = """
@@ -275,23 +285,24 @@ class Database(object):
                 AND ({2}) < end_block_num;
                 """.format(record_public_key, agent_id, LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            try:
-                await cursor.execute(fetch_record)
-                record = await cursor.fetchone()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                try:
+                    await cursor.execute(fetch_record)
+                    record = await cursor.fetchone()
 
-                await cursor.execute(fetch_record_locations)
-                record['locations'] = await cursor.fetchall()
+                    await cursor.execute(fetch_record_locations)
+                    record['locations'] = await cursor.fetchall()
 
-                await cursor.execute(fetch_record_contents)
-                record['contents'] = await cursor.fetchall()
+                    await cursor.execute(fetch_record_contents)
+                    record['contents'] = await cursor.fetchall()
 
-                await cursor.execute(fetch_record_ownership)
-                record['tonnes'] = record['tonnes'] / 100.0 * (await cursor.fetchone())['percentage_owner']
+                    await cursor.execute(fetch_record_ownership)
+                    record['tonnes'] = record['tonnes'] / 100.0 * (await cursor.fetchone())['percentage_owner']
 
-                return record
-            except TypeError:
-                return None
+                    return record
+                except TypeError:
+                    return None
 
     async def fetch_all_record_resources(self):
         fetch_records = """
@@ -301,41 +312,42 @@ class Database(object):
         AND ({0}) < end_block_num;
         """.format(LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            try:
-                await cursor.execute(fetch_records)
-                records = await cursor.fetchall()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                try:
+                    await cursor.execute(fetch_records)
+                    records = await cursor.fetchall()
 
-                for record in records:
-                    fetch_record_locations = """
-                    SELECT latitude, longitude, timestamp
-                    FROM record_locations
-                    WHERE record_id='{0}'
-                    AND published
-                    AND ({1}) >= start_block_num
-                    AND ({1}) < end_block_num;
-                    """.format(record['record_id'], LATEST_BLOCK_NUM)
+                    for record in records:
+                        fetch_record_locations = """
+                        SELECT latitude, longitude, timestamp
+                        FROM record_locations
+                        WHERE record_id='{0}'
+                        AND published
+                        AND ({1}) >= start_block_num
+                        AND ({1}) < end_block_num;
+                        """.format(record['record_id'], LATEST_BLOCK_NUM)
 
-                    fetch_record_owners = """
-                    SELECT agents.id, agents.name, record_owners.percentage_owner 
-                    FROM record_owners
-                    JOIN agents ON agents.public_key = record_owners.agent_id
-                    WHERE record_owners.record_id='{0}'
-                    AND ({1}) >= record_owners.start_block_num
-                    AND ({1}) < record_owners.end_block_num;
-                    """.format(record['record_id'], LATEST_BLOCK_NUM)
+                        fetch_record_owners = """
+                        SELECT agents.id, agents.name, record_owners.percentage_owner 
+                        FROM record_owners
+                        JOIN agents ON agents.public_key = record_owners.agent_id
+                        WHERE record_owners.record_id='{0}'
+                        AND ({1}) >= record_owners.start_block_num
+                        AND ({1}) < record_owners.end_block_num;
+                        """.format(record['record_id'], LATEST_BLOCK_NUM)
 
-                    await cursor.execute(fetch_record_locations)
-                    record['locations'] = await cursor.fetchall()
+                        await cursor.execute(fetch_record_locations)
+                        record['locations'] = await cursor.fetchall()
 
-                    await cursor.execute(fetch_record_owners)
-                    record['owners'] = await cursor.fetchall()
+                        await cursor.execute(fetch_record_owners)
+                        record['owners'] = await cursor.fetchall()
 
-                    del record['record_id']
+                        del record['record_id']
 
-                return records
-            except TypeError:
-                return []
+                    return records
+                except TypeError:
+                    return []
 
 
 
@@ -349,28 +361,29 @@ class Database(object):
         AND ({1}) < records.end_block_num;
         """.format(agent_public_key, LATEST_BLOCK_NUM)
 
-        async with self._conn.cursor(cursor_factory=RealDictCursor) as cursor:
-            try:
-                await cursor.execute(fetch_records)
-                records = await cursor.fetchall()
+        async with self._pool.acquire() as conn:
+            async with conn.cursor(cursor_factory=RealDictCursor) as cursor:
+                try:
+                    await cursor.execute(fetch_records)
+                    records = await cursor.fetchall()
 
-                for record in records:
-                    fetch_record_locations = """
-                    SELECT latitude, longitude, timestamp
-                    FROM record_locations
-                    WHERE record_id='{0}'
-                    AND ({1}) >= start_block_num
-                    AND ({1}) < end_block_num;
-                    """.format(record['record_id'], LATEST_BLOCK_NUM)
+                    for record in records:
+                        fetch_record_locations = """
+                        SELECT latitude, longitude, timestamp
+                        FROM record_locations
+                        WHERE record_id='{0}'
+                        AND ({1}) >= start_block_num
+                        AND ({1}) < end_block_num;
+                        """.format(record['record_id'], LATEST_BLOCK_NUM)
 
-                    await cursor.execute(fetch_record_locations)
-                    record['locations'] = await cursor.fetchall()
+                        await cursor.execute(fetch_record_locations)
+                        record['locations'] = await cursor.fetchall()
 
-                    record['tonnes'] = record['tonnes'] / 100.0 * record['percentage_owner']
+                        record['tonnes'] = record['tonnes'] / 100.0 * record['percentage_owner']
 
-                    del record['record_id']
-                    del record['percentage_owner']
+                        del record['record_id']
+                        del record['percentage_owner']
 
-                return records
-            except TypeError:
-                return []
+                    return records
+                except TypeError:
+                    return []
